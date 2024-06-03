e006f3026bd69fab989a292ce5939b5278201fd6
nanaqisaigao <2804401992@qq.com>
2024-06-03 19:41:32 +0800

    打包了，目前的打包路径是云服务器ip，无法切换注册和登录

--- src/plungins/globalRequest.ts
+++ src/plungins/globalRequest.ts
@@ -13,7 +13,7 @@ import {stringify} from "querystring";
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV === 'production' ? 'http://user-backend.code-nav.cn' : undefined
+  prefix: process.env.NODE_ENV === 'production' ? 'http://47.120.30.71' : undefined
   // requestType: 'form',
 });
 
